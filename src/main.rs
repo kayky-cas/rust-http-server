617de48a015e5411500c0bb054af191496e93354
Kayky Belleboni Casagrande <kbelleboni@gmail.com>
2024-03-15 23:47:14 -0300

    oops wrong path

--- src/main.rs
+++ src/main.rs
@@ -42,7 +42,7 @@ fn main() -> anyhow::Result<()> {
                     .context("reading path")?;
 
                 let response = match &path[..path.len() - 1] {
-                    b"/index.html" => &b"HTTP/1.1 200 OK\r\n\r\n"[..],
+                    b"/" => &b"HTTP/1.1 200 OK\r\n\r\n"[..],
                     _ => &b"HTTP/1.1 404 Not Found\r\n\r\n"[..],
                 };
 
